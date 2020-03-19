f142aac741f0c5c46caf3e3b1dc1be6a9d8b5e68
unknown <jonathangreene888@gmail.com>
2020-03-18 18:16:43 -0600

    passed in just origin for cors issue

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ mongoose.connection
 // setting up middleware
 server.use(express.json());
 server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
